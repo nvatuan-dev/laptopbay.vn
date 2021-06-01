65d69fe80d8d5b3c3854cbcbf0331fe3319ca8e5
Phuoc <phuoc.ha@fman.tech>
2021-06-01 19:43:59 +0700

    update frontend

--- components/RoundedList.vue
+++ components/RoundedList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="dark:bg-gray-900 py-1 flex flex-col justify-center sm:py-4">
-        <div class="grid grid-cols-4 gap-2 px-2">
+        <div class="grid grid-cols-4 gap-2 md:gap-8 mx-2 md:mx-4">
             <!-- SMALL CARD ROUNDED -->
             <div class="bg-gray-100 border-indigo-600 dark:bg-gray-800 bg-opacity-95 border-opacity-60 | p-2 md:p-4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-indigo-400 dark:hover:bg-indigo-600 hover:border-transparent | transition-colors duration-500">
                 <img class="w-4 h-4 md:w-8 md:h-8 object-cover" src="https://milenyo.net/wp-content/uploads/2020/05/netflix-n-logo-png.png" alt="" />
